c113a1801258d52abf1c0c74b536134d5aa0cf05
JiaWeiHsu <bryan35818363680919@gmail.com>
2019-01-03 14:15:07 +0800

    update main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ with picamera.PiCamera() as camera:
     camera.start_preview()
     time.sleep(2)
     while True:
-        
         camera.capture_sequence([
             'image/0.jpg',
             ], use_video_port=True)
         
+        func.countObj('image/0.jpg')
+
     camera.stop_preview()
