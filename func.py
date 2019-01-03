5308a75fd307096aacdb01546e3c1d722675b90d
JiaWeiHsu <bryan35818363680919@gmail.com>
2019-01-03 18:17:19 +0800

    add img

--- func.py
+++ func.py
@@ -15,48 +15,17 @@ def get_frame():
     img = open("image/0.jpg", "rb").read()
     return img
 
-
-def sendEmail(img, to_addr):
-    cf = ConfigParser.ConfigParser()
-    cf.read("email.conf")
-
-    gmail_user = cf.get("email", "username")
-    gmail_password = cf.get("email", "password")
-    Subject = "標題"
-
-    msgRoot = MIMEMultipart('related')
-    msgRoot['Subject'] = Subject
-    msgRoot['From'] = gmail_user
-    msgRoot['To'] = to_addr
-    
-    fp = open(img, 'rb')
-    msgImage = MIMEImage(fp.read())
-    fp.close()
-    msgImage.add_header('Content-Disposition', 'attachment', filename=os.path.basename(img))
-    msgRoot.attach(msgImage)
-   
-    msg=msgRoot.as_string()
-
-    smtp=smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    smtp.ehlo()
-    smtp.login(gmail_user, gmail_password)
-    status=smtp.sendmail(gmail_user, to_addr, msg)
-    if status=={}:
-        print("郵件傳送成功!")
-    else:
-        print("郵件傳送失敗!")
-    smtp.quit()
-
-
-def countObj(imgName):
+def countObj():
 
     if os.stat('image/0.jpg').st_size > 0:
 
         fidelity = False
         fidelityValue = .7
-        img = cv2.imread(imgName)
+        img = cv2.imread('image/0.jpg')
         imgCopy = img.copy()
         img = cv2.medianBlur(img, 15)
+        #img = cv2.adaptiveBilateralFilter(img, (5, 5), 150) # Preserve edges
+        #img = cv2.blur(img, (3,3))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         #imgt = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 3, 10)
         _, imgt = cv2.threshold(img, 125, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
@@ -76,20 +45,20 @@ def countObj(imgName):
 
         totalContours = 0
 
-        # br = []
+        br = []
         for i in xrange(len(c)):
             if h[0][i][3] == ROOT_NODE and cv2.contourArea(c[i]) >= fidelityRange:
                 totalContours += 1
-                # approx = cv2.approxPolyDP(c[i], 3, True)
-                # br.append(cv2.boundingRect(approx))
-        # for b in br:
-            # cv2.rectangle(imgCopy, (b[0], b[1]), (b[0] + b[2], b[1] + b[3]), (255, 255, 0), 3)
-        # cv2.imshow('image',imgCopy)
-        # cv2.waitKey(0)
-        print totalContours
+                approx = cv2.approxPolyDP(c[i], 3, True)
+                br.append(cv2.boundingRect(approx))
+        for b in br:
+            cv2.rectangle(imgCopy, (b[0], b[1]), (b[0] + b[2], b[1] + b[3]), (255, 255, 0), 3)
+        cv2.imwrite('image/1.jpg', imgCopy)
+        print 'Total contours: ', totalContours
         return totalContours
 
     else:
         print(-1)
         return -1
+
 
--- test.py
+++ test.py
@@ -6,7 +6,6 @@ with picamera.PiCamera() as camera:
     camera.start_preview()
     time.sleep(2)
     while True:
-
         camera.capture_sequence([
             'image/0.jpg',
             ], use_video_port=True)
